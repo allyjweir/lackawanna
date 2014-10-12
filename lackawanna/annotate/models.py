026e5faa531a1f732b0e384afc63f8431415dc35
Ally Weir <ally.pcgf@gmail.com>
2014-10-12 23:46:09 +0100

    Annotate model added

--- lackawanna/annotate/models.py
+++ lackawanna/annotate/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
-# Create your models here.
+
+class Annotation(models.Model):
+    datapoint = models.ForeignKey('datapoint.Datapoint', related_name = 'parent datapoint relation')
+    owner = models.ForeignKey('users.User', related_name = 'creator relation')
+
+    # Key fields from the Annotator JSON Format: http://docs.annotatorjs.org/en/latest/annotation-format.html
+    text = models.CharField(blank = True)
+    quote = models.CharField
+    uri = models.URLField(blank = True)
+    range_start = models.CharField(max_length = 50)
+    range_end = models.CharField(max_length = 50)
+    range_startOffset = models.BigIntegerField(blank = True)
+    range_endOffset = models.BigIntegerField(blank = True)
+    tags = TaggableManager(blank = True)
+
+    # Created/Modified
+    # See this for background: http://stackoverflow.com/questions/1737017/django-auto-now-and-auto-now-add/1737078#1737078
+    created     = models.DateTimeField(editable = False)
+    modified    = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.created = datetime.datetime.today()
+        self.modified = datetime.datetime.today()
+        return super(Datapoint, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return self.datapoint.name + ":'" + self.quote + "'"
