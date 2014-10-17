34df78e0a61d8ad0b6ada917c09a40fb4a9587de
Ally Weir <ally.pcgf@gmail.com>
2014-10-17 16:15:17 +0100

    Implemented generic views for Collection app

--- lackawanna/collection/urls.py
+++ lackawanna/collection/urls.py
@@ -2,13 +2,23 @@ from django.conf.urls import patterns, url
 from collection import views
 
 urlpatterns = patterns('',
-        # Index (List of Projects)
+        # Index (List of Collections)
         url(regex=r'^$',
-            view=views.index,
+            view=views.CollectionListView.as_view,
             name='index'),
 
         # Collection Overview/Dashboard
-        url(regex=r'^(?P<slug>\w+)/$',
-            view=views.collection_page,
-            name='page'),
+        #url(regex=r'^(?P<slug>\w+)/$',
+        #    view=views.collection_page,
+        #    name='page'),
+
+        # Create Collection
+        url(regex=r'^create/$',
+            view=views.CollectionCreateView.as_view(),
+            name='create'),
+
+        # Update Collection
+        url(regex=r'^update/$',
+            view=views.CollectionUpdateView.as_view(),
+            name='update'),
 )
--- lackawanna/collection/views.py
+++ lackawanna/collection/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
-
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import CreateView, UpdateView, ListView, DeleteView
 from django.http import HttpResponse
 
+from braces.views import LoginRequiredMixin
 
-def index(request):
-    return HttpResponse("Collection index page here!")
+from .models import Collection
+
+class CollectionListView(LoginRequiredMixin, ListView):
+    model = Collection
 
 
-def collection_page(request):
+class CollectionCreateView(LoginRequiredMixin, CreateView):
+    model = Collection
+    fields = ('owner', 'project', 'name', 'description',)
+    success_url = reverse_lazy('project:list')
+
+
+class CollectionUpdateView(LoginRequiredMixin, UpdateView):
+    model = Collection
+    fields = ('owner', 'project', 'name', 'description',)
+    success_url = reverse_lazy('collection:list')
+
+
+class ProjectDeleteView(LoginRequiredMixin, DeleteView):
+    model = Collection
+    success_url = reverse_lazy('collection:list')
+
+
+def detail(request):
     return HttpResponse("Individual collection pages go here!")
