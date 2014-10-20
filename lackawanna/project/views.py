1ae0903c47534774e59258267ed0654b9e1e9b91
Ally Weir <ally.pcgf@gmail.com>
2014-10-20 17:17:01 +0100

    Created test page for individual project pages.
    Still using function based view here. Hope to change this to class based view later in a refactor but for now will leave it as FBV. This lets me iterate quicker and avoid fighting with the framework so much

--- lackawanna/project/views.py
+++ lackawanna/project/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import CreateView, UpdateView, ListView, DeleteView
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 from braces.views import LoginRequiredMixin
 
 from .models import Project
+from collection.models import Collection
 
 class ProjectListView(LoginRequiredMixin, ListView):
     model = Project
@@ -29,5 +32,15 @@ class ProjectDeleteView(LoginRequiredMixin, DeleteView):
 
 
 def detail(request, slug):
-    return HttpResponse("Individual project pages go here!")
+    context = RequestContext(request)
+    context_dict = {'slug':slug}
+    try:
+        project = Project.objects.get(slug=slug)
+        context_dict['project'] = project
 
+        collections = Collection.objects.filter(project=project)
+        context_dict['collections'] = collections
+    except Project.DoesNotExist:
+        return HttpResponse("No project called this")
+
+    return render_to_response('project/project_detail.html', context_dict, context)
