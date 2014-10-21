ba14c23f3713f66f34d1a6b64f662984b1b52b0d
Ally Weir <ally.pcgf@gmail.com>
2014-10-21 23:38:01 +0100

    Added statistics for stat grabs

--- lackawanna/dashboard/views.py
+++ lackawanna/dashboard/views.py
@@ -7,12 +7,23 @@ from braces.views import LoginRequiredMixin
 
 from project.models import Project
 from collection.models import Collection
+from datapoint.models import Datapoint
+from users.models import User
+
 
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = "dashboard/dashboard.html"
 
     def get_context_data(self, **kwargs):
         context = super(DashboardView, self).get_context_data(**kwargs)
+
+        #Sidebar Recent lists
         context['latest_projects'] = Project.objects.all()[:3]
         context['latest_collections'] = Collection.objects.all()[:3]
+
+        #grabstats
+        context['users_count'] = User.objects.count()
+        context['project_count'] = Project.objects.count()
+        context['datapoint_count'] = Datapoint.objects.count()
+
         return context
