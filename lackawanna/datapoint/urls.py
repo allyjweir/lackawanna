8f529e7600d2f1baa35c390a6e23fc95f073c9bf
Ally Weir <ally.pcgf@gmail.com>
2014-10-29 22:49:24 +0000

    User now makes a choice between uploading a file or a web based piece of information.
    This is to allow me to offer two different forms based on this. For files it will be a pretty standard form, for websites however it will ask for a URL, you submit that and then Lackawanna will fetch the information from the website itself in the background.

--- lackawanna/datapoint/urls.py
+++ lackawanna/datapoint/urls.py
@@ -12,6 +12,15 @@ urlpatterns = patterns('',
             view=views.DatapointUploadView.as_view(),
             name='upload'),
 
+        # Upload File
+        url(regex=r'^upload/file/$',
+            view=views.DatapointFileUploadView.as_view(),
+            name='upload_file'),
+
+        # Upload Webpage
+        url(regex=r'^upload/web/$',
+            view=views.DatapointWebUploadView.as_view(),
+            name='upload_web'),
 
         # Update Datapoint
         url(regex=r'^(?P<pk>[\w.@+-]+)/update/$',
