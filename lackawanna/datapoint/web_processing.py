016bbc498b93625a067d8c394635adcc8d9ea83a
Ally Weir <ally.pcgf@gmail.com>
2014-11-06 21:59:02 +0000

    Future work noted for web datapoint processing

--- lackawanna/datapoint/web_processing.py
+++ lackawanna/datapoint/web_processing.py
@@ -7,13 +7,14 @@ def process_website(url):
     a.parse()
     a.nlp()
 
+    article={}
     article['authors']=a.authors
     article['title']=a.title
     article['summary']=a.summary
     article['lead_image']=a.top_image
-    article['images']=a.images
+    #article['images']=a.images - Also returns random images from the page so will not be returned
     article['movies']=a.movies
     article['text']=a.text
-    article['keywords']=a.keywords
+    #article['keywords']=a.keywords - Newspaper's keyword extraction isn't great so a more specialised library will be used. To be added
 
     return article
