b013a5759b04d3ec8bc0f08cf0bfb9bb925d3048
Ally Weir <ally.pcgf@gmail.com>
2014-11-10 19:36:23 +0000

    Added authentication to the REST API
    BasicAuthentication allows connections from within Django with a Django user object logged in.
    SessionAuthentication is there to support AJAX calls to the API. It authenticates through use of the session ID and CSRF token stored in a user's cookies.

--- lackawanna/config/common.py
+++ lackawanna/config/common.py
@@ -277,11 +277,20 @@ class Common(Configuration):
     # END LOGGING CONFIGURATION
 
     REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-        'DEFAULT_PERMISSION_CLASSES': [
-            'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-        ]
+        'DEFAULT_AUTHENTICATION_CLASSES': (
+            'rest_framework.authentication.BasicAuthentication',
+            'rest_framework.authentication.SessionAuthentication',
+        ),
+
+
+        'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+        ),
+
+
+        
+
+
     }
 
     # django-markupfield stuff
