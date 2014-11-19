29ea69a3fdbe39217033bc4c18bda2274959dc3d
Ally Weir <ally.pcgf@gmail.com>
2014-11-19 13:30:35 +0000

    Created a helper function file in core

---
+++ lackawanna/core/helpers.py
@@ -0,0 +1,12 @@
+import os
+import errno
+
+
+def make_sure_path_exists(path):
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+
+# TODO: Function that will delete a temporary file given a directory
