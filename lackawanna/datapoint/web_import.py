413bbd1f896f4a75489f3a828e51fe8da7a85828
Ally Weir <ally.pcgf@gmail.com>
2014-11-19 11:43:17 +0000

    Combined and simplified web extraction code

---
+++ lackawanna/datapoint/web_import.py
@@ -0,0 +1,66 @@
+import newspaper
+from newspaper import Article
+import requests
+from bs4 import BeautifulSoup
+from topia.termextract import extract
+from selenium import webdriver
+
+
+
+def get_article(url):
+    a = Article(url)
+    a.download()
+    a.parse()
+    a.nlp()
+
+    article={}
+    article['authors']=a.authors
+    article['title']=a.title
+    article['summary']=a.summary
+    article['lead_image']=a.top_image
+    article['movies']=a.movies
+    article['text']=a.text
+    article['keywords']=get_keywords(a.text)
+
+    return article
+
+
+'''This calls out to PhantomJS through Selenium WebDriver to capture a PNG of a web page'''
+def get_screenshot(url):
+    web = webdriver.PhantomJS()
+    web.set_window_size(1280,1080)
+    web.get(url)
+    #web.save_screenshot() TODO: Save to temporary location. Check Django docs for this loc.
+    web.quit()
+    return True
+
+
+'''Using the Toperia Term Extractor, this finds keywords, along with their importance to the text as a whole, and returns them'''
+def get_keywords(text):
+    extractor = extract.TermExtractor()
+    return sorted(extractor(text))
+
+
+'''
+Basically from researching it I cannot find a library that when given a URL of a tweet, it can go and find the text, timestamp and username of that tweet. This hack solves that. It is dangerous and likely to become outmoded in the future but is at least easily testable. It will do the task okay for the project as long as it is backed up by the PNG/PDF of the page itself.
+The Twitter API itself seems to be more about creating clients. There seems to be a lot of hoops for just retrieving a singular tweet in (for argument's sake) JSON. You require authentication, it is rate limited etc. It can do it (as seen here https://dev.twitter.com/rest/reference/get/statuses/show/%3Aid but I'm not sure if it is worth all of that hassle...yet. I am sure the perfectionist in me will make me want to sort it out properly!)
+'''
+def get_tweet(url):
+    # Retrieve tweet
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text)
+
+    # Find it in the soup
+    acc_name = soup.find_all('strong', 'fullname')
+    username = soup.find_all('span', 'username js-action-profile-name')
+    tweet_text = soup.find_all('p', 'js-tweet-text')
+    tweet_timestamp = soup.find_all('a', 'tweet-timestamp')
+
+    # Process it to get something intelligible
+    tweet={}
+    tweet['text'] = tweet_text[0].get_text().encode('ascii', 'ignore')
+    tweet['timestamp'] = tweet_timestamp[0]['title']
+    tweet['username'] = username[0].get_text().encode('ascii', 'ignore')
+    tweet['name'] = acc_name[0].get_text().encode('ascii', 'ignore')
+
+    return tweet
