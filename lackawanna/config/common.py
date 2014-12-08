ebc2b38e5b362a552957d96c1379af2449354c71
Ally Weir <ally.pcgf@gmail.com>
2014-12-08 20:49:05 +0000

    Cleanup of settings file

--- lackawanna/config/common.py
+++ lackawanna/config/common.py
@@ -288,21 +288,4 @@ class Common(Configuration):
         'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
         ),
-
-
-        
-
-
     }
-
-    # django-markupfield stuff
-    import markdown
-    from docutils.core import publish_parts
-
-    def render_rest(selfmarkup):
-        parts = publish_parts(source=markup, writer_name="html4css1")
-        return parts["fragment"]
-
-    MARKUP_FIELD_TYPES = (
-        ('markdown', markdown.markdown),
-    )
