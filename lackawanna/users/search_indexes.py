8759b5c3b89a6e6eca9b5b385a30e8914d9204eb
Ally Weir <ally.pcgf@gmail.com>
2015-01-29 09:39:49 +0000

    Added users to search index

---
+++ lackawanna/users/search_indexes.py
@@ -0,0 +1,16 @@
+import datetime
+from haystack import indexes
+from .models import User
+
+class UserIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    first_name = indexes.CharField(model_attr="first_name")
+    last_name = indexes.CharField(model_attr="last_name")
+    username = indexes.CharField(model_attr="username")
+    email_address = indexes.CharField(model_attr='email')
+    def get_model(self):
+        return User
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.all()
