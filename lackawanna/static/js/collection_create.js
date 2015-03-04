5add9c745bc89b7e7e4ed51e4c16c4b1222516f2
Ally Weir <ally.pcgf@gmail.com>
2015-03-04 13:06:20 +0000

    Autofill happens on collection creation from within a project

---
+++ lackawanna/static/js/collection_create.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    var project_pk = getQueryVariable('project');
+    $("#id_project").val(project_pk);
+
+});
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    console.log('Query variable %s not found', variable);
+}
