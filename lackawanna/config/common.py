fb60a3500366f881ab198ffb9be370b6eecbaaef
Ally Weir <ally.pcgf@gmail.com>
2015-04-22 15:03:45 +0100

    Can log in with username OR email.
    CodingHorror's suggestions

--- lackawanna/config/common.py
+++ lackawanna/config/common.py
@@ -228,7 +228,7 @@ class Common(Configuration):
     )
 
     # Some really nice defaults
-    ACCOUNT_AUTHENTICATION_METHOD = "username"
+    ACCOUNT_AUTHENTICATION_METHOD = "username_email"
     ACCOUNT_EMAIL_REQUIRED = True
     ACCOUNT_EMAIL_VERIFICATION = "none"
     # END AUTHENTICATION CONFIGURATION
